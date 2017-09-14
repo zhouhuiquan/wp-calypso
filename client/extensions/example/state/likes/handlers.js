4d75e96b870184b87b169c2741c170ccc1d4e1ae
Kevin Killingsworth <kevin@redfenix.com>
2017-09-13 21:47:59 -0700

    Extensions example: use dispatchRequest
    This adjusts the http call to use dispatchRequest so the resulting
    action can be set with the actual resulting data.
    
    To Test:
    1. Visit http://calypso.localhost:3000/extensions/example
    2. Open Redux DevTools
    3. Filter for actions by EXAMPLE
    4. Look at resulting data in last action in list.

--- client/extensions/example/state/likes/handlers.js
+++ client/extensions/example/state/likes/handlers.js
@@ -2,26 +2,43 @@
  * Internal dependencies
  */
 import { http } from 'state/data-layer/wpcom-http/actions';
+import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import {
 	EXAMPLE_FETCH_LIKES,
 	EXAMPLE_LIKES_UPDATED,
 } from 'example/state/action-types';
 
 export default {
-	[ EXAMPLE_FETCH_LIKES ]: [ fetchLikes ],
+	[ EXAMPLE_FETCH_LIKES ]: [
+		dispatchRequest(
+			fetchLikes, likesFetched, likesFailure
+		)
+	],
 };
 
 function fetchLikes( { dispatch }, action ) {
+	dispatch(
+		http(
+			{
+				method: 'GET',
+				apiVersion: '1.1',
+				path: '/me/likes/',
+			},
+			action
+		)
+	);
+}
+
+function likesFetched( { dispatch }, action, data ) {
+	dispatch( {
+		type: EXAMPLE_LIKES_UPDATED,
+		data,
+	} );
+}
+
+function likesFailure( { dispatch }, action ) {
 	const { failureAction } = action;
 
-	dispatch( http( {
-		method: 'GET',
-		apiVersion: '1.1',
-		path: '/me/likes/',
-		onSuccess: {
-			type: EXAMPLE_LIKES_UPDATED,
-		},
-		onFailure: failureAction,
-	} ) );
+	dispatch( failureAction );
 }
 
