341740efb317fd54388ab5efaf53d9b35e558a8d
budzanowski <bartosz.budzanowski@gmail.com>
2017-12-11 13:51:22 +0100

    Format code.

--- client/extensions/woocommerce/app/settings/email/email-settings/components/customer-notification.js
+++ client/extensions/woocommerce/app/settings/email/email-settings/components/customer-notification.js
@@ -1,3 +1,5 @@
+/** @format */
+
 /**
  * External dependencies
  */
@@ -15,41 +17,34 @@ import ListItemField from 'woocommerce/components/list/list-item-field';
 
 const CustomerNotification = ( { item, checked, onChange, isPlaceholder } ) => {
 	//Add field name to returned value
-	const toggle = ( value ) => {
-		onChange(
-			{
-				setting: item.field,
-				field: 'enabled',
-				value: value ? 'yes' : 'no',
-			}
-		);
+	const toggle = value => {
+		onChange( {
+			setting: item.field,
+			field: 'enabled',
+			value: value ? 'yes' : 'no',
+		} );
 	};
 
 	return (
 		<ListItem className="components__notification-component-item">
 			<ListItemField className="components__notification-component-title-long">
-				{ ! isPlaceholder
-					? <FormLabel>
-						{ item.title }
-					</FormLabel>
-					: <p className="components__is-placeholder" />
-				}
-				{ ! isPlaceholder
-					? <FormSettingExplanation>
-						{ item.subtitle }
-					</FormSettingExplanation>
-					: <p className="components__is-placeholder" />
-				}
+				{ ! isPlaceholder ? (
+					<FormLabel>{ item.title }</FormLabel>
+				) : (
+					<p className="components__is-placeholder" />
+				) }
+				{ ! isPlaceholder ? (
+					<FormSettingExplanation>{ item.subtitle }</FormSettingExplanation>
+				) : (
+					<p className="components__is-placeholder" />
+				) }
 			</ListItemField>
 			<ListItemField className="components__notification-component-toggle">
-				{ ! isPlaceholder
-					? <CompactFormToggle
-						checked={ checked }
-						onChange={ toggle }
-						id={ item.field }
-					/>
-					: <p className="components__is-placeholder" />
-				}
+				{ ! isPlaceholder ? (
+					<CompactFormToggle checked={ checked } onChange={ toggle } id={ item.field } />
+				) : (
+					<p className="components__is-placeholder" />
+				) }
 			</ListItemField>
 		</ListItem>
 	);
@@ -61,4 +56,4 @@ CustomerNotification.propTypes = {
 	onChange: PropTypes.func.isRequired,
 };
 
-export default CustomerNotification ;
+export default CustomerNotification;
