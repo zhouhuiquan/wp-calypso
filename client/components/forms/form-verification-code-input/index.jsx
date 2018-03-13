8fbb503641457188b0f9ff18190e377564dfb9c7
Stéphane Thomas <email@stephanethomas.com>
2018-03-13 16:09:40 +0100

    Login: Convert Form Verification Code Input component to stateful
    This fixes 'Stateless function components cannot be given refs' and 'Uncaught TypeError: _this2.input.focus is not a function' errors.

--- client/components/forms/form-verification-code-input/index.jsx
+++ client/components/forms/form-verification-code-input/index.jsx
@@ -12,40 +12,53 @@ import React from 'react';
  */
 import constants from 'me/constants';
 
-export default function FormVerificationCodeInput( { className, isError, isValid, method, ...otherProps } ) {
-	const classes = classNames( 'form-verification-code-input', className, {
-		'is-error': isError,
-		'is-valid': isValid,
-	} );
+export default class FormVerificationCodeInput extends React.Component {
+	static propTypes = {
+		className: PropTypes.string,
+		isError: PropTypes.bool,
+		isValid: PropTypes.bool,
+		method: PropTypes.string,
+	};
 
-	let placeholder = constants.sixDigit2faPlaceholder;
+	static defaultProps = {
+		isError: false,
+		isValid: false,
+	};
 
-	if ( method === 'backup' ) {
-		placeholder = constants.eightDigitBackupCodePlaceholder;
-	} else if ( method === 'sms' ) {
-		placeholder = constants.sevenDigit2faPlaceholder;
+	focus = () => {
+		this.input.focus();
+	};
+
+	saveRef = input => {
+		this.input = input;
+	};
+
+	render() {
+		const { className, isError, isValid, method, ...otherProps } = this.props;
+
+		const classes = classNames( 'form-verification-code-input', className, {
+			'is-error': isError,
+			'is-valid': isValid,
+		} );
+
+		let placeholder = constants.sixDigit2faPlaceholder;
+
+		if ( method === 'backup' ) {
+			placeholder = constants.eightDigitBackupCodePlaceholder;
+		} else if ( method === 'sms' ) {
+			placeholder = constants.sevenDigit2faPlaceholder;
+		}
+
+		return (
+			<input
+				autoComplete="off"
+				className={ classes }
+				pattern="[0-9 ]*"
+				placeholder={ placeholder }
+				ref={ this.saveRef }
+				type="tel"
+				{ ...otherProps }
+			/>
+		);
 	}
-
-	return (
-		<input
-			autoComplete="off"
-			className={ classes }
-			pattern="[0-9 ]*"
-			placeholder={ placeholder }
-			type="tel"
-			{ ...otherProps }
-		/>
-	);
 }
-
-FormVerificationCodeInput.propTypes = {
-	className: PropTypes.string,
-	isError: PropTypes.bool,
-	isValid: PropTypes.bool,
-	method: PropTypes.string,
-};
-
-FormVerificationCodeInput.defaultProps = {
-	isError: false,
-	isValid: false,
-};
